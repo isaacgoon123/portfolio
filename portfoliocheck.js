b3413dc5a4de7e243b7b00aaaee2171b23cecafc
isaacgoon123 <61223660+isaacgoon123@users.noreply.github.com>
2020-03-02 17:45:31 +0800

    Add files via upload

--- portfoliocheck.js
+++ portfoliocheck.js
@@ -1,6 +1,29 @@
 $(document).ready(function () {
     $('.menu-toggler').on('click', function () {
         $(this).toggleclass('open');
-        $('.top').toggleclass('open');
+        $('.top-nav').toggleclass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeclass('open');
+        $('.top-nav').removeclass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate(keyframes:{
+            scrolltop: $($(this).attr('href')).offset().top - 100
+        }, ptions: 2000);
+    });
+
+    $('#up').on('click', function () {
+        $('html, body').animate(keyframes: {
+            scrolltop: 0
+        }, Options: 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1000,
+        once: true
     });
 });
