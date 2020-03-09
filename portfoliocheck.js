0672796fc539393550e14d79edf9f7c5659460e9
isaacgoon123 <61223660+isaacgoon123@users.noreply.github.com>
2020-03-09 17:44:59 +0800

    Add files via upload

--- portfoliocheck.js
+++ portfoliocheck.js
@@ -1,25 +1,30 @@
 $(document).ready(function () {
-    $('.menu-toggler').on('click', function () {
-        $(this).toggleclass('open');
-        $('.top-nav').toggleclass('open');
+    
+    $('#hamburger').on('click', function() {
+    
+       $(this).toggleClass('menu-toggler open');
+        $('.top-nav').toggleClass('menu-toggler open');
+        
     });
 
-    $('.top-nav .nav-link').on('click', function () {
-        $('.menu-toggler').removeclass('open');
-        $('.top-nav').removeclass('open');
-    });
+    // $('.top-nav .nav-link').on('click', function () {
+    //     $('.menu-toggler').removeclass('open');
+    //     $('.top-nav').removeclass('open');
+    // });
 
     $('nav a[href*="#"]').on('click', function () {
-        $('html, body').animate(keyframes:{
+        $('html, body').animate( {
             scrolltop: $($(this).attr('href')).offset().top - 100
-        }, ptions: 2000);
+        },  2000);
     });
 
     $('#up').on('click', function () {
-        $('html, body').animate(keyframes: {
+        $('html, body').animate( {
             scrolltop: 0
-        }, Options: 2000);
+        }, 2000);
     });
+
+
 
     AOS.init({
         easing: 'ease',
